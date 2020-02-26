6919e57e17c66defaa09fdcb4dad05a86cd0d05d
Colton stephens <stephens.colton@icloud.com>
2020-02-26 22:51:59 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -3,8 +3,6 @@ BASE_HOA = {
   :third_earthers => ["Lion-O", "Cheetara", "Mumm-Ra (the ever-living)"],
   :jetsons => ["George", "Jane", "Judy", "Elroy"]
 }
-BASE_HOA[:chipmunks]
-<<"Dave"
 
 
 def add_character(show, name)
